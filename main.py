8e0faccb790aa8ba5ae3fe28585e2012b1a5bfcc
George <george_foster2012@hotmail.co.uk>
2026-01-23 13:18:35 +0000

    colour region opencv testing

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from window_util import Window
 
 def main():
     window = Window()
-    window.find(title="RuneLite", exact_match=False)
+    window.find(title="RuneLite - xJawj", exact_match=True)
     if window.window:
         window.capture(debug=True)
-        found = window.find_color_region((255, 0, 0), tolerance=25, debug=True)
+        found = window.find_color_region((190, 25, 25), tolerance=0, debug=True)
         # mouse_color = window.get_color_at_mouse()
         # print(f"Color at mouse position: {mouse_color}")
         if found:
@@ -14,11 +14,13 @@ def main():
             print(f"Moving mouse to: {found.center()}")
             window.move_mouse_to(found.center())
             sleep(1)
-            window.move_mouse_to(found.random_point())
+            window.move_mouse_to((found.x, found.y))
             sleep(1)
-            window.move_mouse_to(found.random_point())
+            window.move_mouse_to((found.x + found.width, found.y))
             sleep(1)
-            window.move_mouse_to(found.random_point())
+            window.move_mouse_to((found.x + found.width, found.y + found.height))
+            sleep(1)
+            window.move_mouse_to((found.x, found.y + found.height))
         else:
             print("Color not found.")
     
